cff6f75be568630b63ab6f5be6977ade6725867f
runsf <mclausaudio@gmail.com>
2018-08-15 17:10:19 +0000

    added jquery and started html + css

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
       
       
 app.get('/', function(req, res){
-    res.send("HI from expres");
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
---
+++ public/app.js
@@ -0,0 +1,1 @@
+alert('connect to app.js')
