66f1560a7d0edef5b67ba5d5b9645bf43674e43b
runsf <mclausaudio@gmail.com>
2018-08-15 16:46:53 +0000

    refactor api, added helper functions and cleaned up routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,57 +1,15 @@
 const express = require("express"),
 router = express.Router(),
-db = require('../models')
+db = require('../models'),
+helpers = require('../helpers/todos');
 
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo);
 
-router.get('/', function(req, res){
-    db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-router.post('/', function(req, res){
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-router.get('/:todoId', function(req, res){
-    db.Todo.findById(req.params.todoId)
-    .then(function(foundTodo){
-        res.json(foundTodo)
-    })
-    .catch(function(err){
-        res.sned(err)
-    })
-});
-
-router.put('/:todoId', function (req, res){
-    //adding {new: true} makes mongoose send the updated version rather than the old vers
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-    .then(function(todo){
-        res.json(todo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-router.delete('/:todoId', function(req, res){
-    db.Todo.remove({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: "Successfully deleted, nice one."})
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo);
 
 module.exports = router;
