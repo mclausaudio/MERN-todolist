f959d051a92da98dc1fa618b3d98448fd91d9c61
runsf <mclausaudio@gmail.com>
2018-09-12 16:23:03 +0000

    added listeners and ajax calls, added mLab

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://mikec:todoapp1@ds155292.mlab.com:55292/todoapp');
+
+// mongoose.connect('mongodb://localhost/todo-api');
 
 mongoose.Promise = Promise;
 
--- public/app.js
+++ public/app.js
@@ -1,1 +1,91 @@
-alert('connect to app.js')
+/*global $ */
+
+$(document).ready(function(){
+   $.getJSON('/api/todos')
+   .then(function(data){
+       addTodos(data);
+   })
+   .catch(function(err){
+     alert(err);
+   });
+   
+   
+   $('#todoInput').keypress(function(event){
+     if (event.which == 13) {
+       createTodo();
+     }
+   });
+   
+  $('.list').on('click', 'li', function(){
+    updateTodo($(this));
+  })
+   
+   $('.list').on('click', 'span', function(event) {
+     //event.stropPropagation() prevents li click handler from firing - 'event bubbling'
+    event.stopPropagation();
+    removeTodo($(this).parent());
+  });
+  
+});
+
+function addTodos(todos) {
+    todos.forEach(function(todo){
+      addTodo(todo);
+    });
+}
+
+function addTodo(todo){
+    var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
+    //NOTE: .data() is a jquery function that allows us to store data in an element, for example mongose ID
+    newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
+      if(todo.completed) {
+        newTodo.addClass("done");
+      }
+      $('.list').append(newTodo);
+}
+
+function createTodo() {
+  //send request to create new todo
+  var userInput = $('#todoInput').val();
+  
+  $.post('/api/todos', {name: userInput})
+  .then(function(newTodo){
+    addTodo(newTodo);
+    $('#todoInput').val('');
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+}
+
+function removeTodo(todo){
+    var clickedId = todo.data('id');
+    var deleteUrl = '/api/todos/' + clickedId;
+    $.ajax({
+      method: 'DELETE',
+      url: deleteUrl
+    })
+    .then(function(data){
+      todo.remove();
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+function updateTodo(todo) {
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone};
+  console.log(updateData)
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    todo.toggleClass('done');
+    todo.data('completed', isDone);
+  })
+}
